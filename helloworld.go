80c794df576ed9b7fc9246f99622fb3bbb68d701
powersplay <dan.powers@docker.com>
2015-03-24 17:00:08 -0700

    liking number 3

--- helloworld.go
+++ helloworld.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("hello world too")
-	fmt.Println("hello world three")
+	fmt.Println("hello world 3")
 	fmt.Println("hello world four")
 }
